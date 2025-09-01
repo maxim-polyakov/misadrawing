4fc4a2a5bb8afdcb07717eac807d99af3adeb21b
maxim-polyakov <maxim7012@gmail.com>
2025-09-01 20:33:33 +0300

    Update

--- server/src/app/routes/index.js
+++ server/src/app/routes/index.js
@@ -1,9 +1,7 @@
 const Router = require('express')
-const messageRouter = require('./messsageRouter')
 const userRouter = require('./userRouter')
 const router = new Router()
 
-router.use('/messages', messageRouter)
 router.use('/user', userRouter)
 
 module.exports = router
--- server/src/app/routes/messsageRouter.js
+++
@@ -1,8 +0,0 @@
-const Router = require('express')
-const router = new Router()
-const messageController = require('../controllers/messageController')
-
-router.post('/', messageController.send)
-router.get('/', messageController.recieve)
-
-module.exports = router
