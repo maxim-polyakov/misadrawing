6aaf6ffb0fe015699f3a1e5faa2916ab3e94fb91
maxim-polyakov <maxim7012@gmail.com>
2026-03-01 20:11:57 +0300

    Update imageApi.js

--- client/src/http/imageApi.js
+++ client/src/http/imageApi.js
@@ -9,7 +9,6 @@ export const uploadImage = async (file) => {
     const formData = new FormData();
     formData.append("image", file);
     const { data } = await $authhost.post("api/images/upload", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
         timeout: 120000,
     });
     return data;
@@ -19,7 +18,6 @@ export const uploadBackground = async (file) => {
     const formData = new FormData();
     formData.append("image", file);
     const { data } = await $authhost.post("api/images/upload-background", formData, {
-        headers: { "Content-Type": "multipart/form-data" },
         timeout: 120000,
     });
     return data;
